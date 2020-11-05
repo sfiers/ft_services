a9a5b2c3209d7db5064a414b7a740d40f55570e6
Simon Simon <ssimon@shi-r11-p3.s19.be>
2020-11-05 18:11:21 +0100

    phpmyadmin is working

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 docker rm -f $(docker ps -aq)
-docker rmi mysql:0
-docker build -t mysql:0 ./srcs/mysql/
-docker run --name mysql -it mysql:0
+# docker rmi mysql:0
+# docker build -t mysql:0 ./srcs/mysql/
+# docker run --name mysql -it mysql:0
+
+# docker build -t nginx:0 ./srcs/nginx/
+# docker run --name nginx -it -p 80:80 nginx:0
+
+docker build -t phpmyadmin:0 ./srcs/phpmyadmin/
+docker run --name phpmyadmin -d -p 5000:5000 phpmyadmin:0
